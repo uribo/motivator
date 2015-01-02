a5abaad260a5652de15c7eac4af890eac1063fdf
Shinya Uryu <suika1127@gmail.com>
2015-01-02 16:25:24 +0900

    [change]I did my best

--- R/shinchoku_test.R
+++ R/shinchoku_test.R
@@ -5,7 +5,7 @@
 #' @import yeah
 #' @param goal
 #' @export
-shinchoku_test <- function (goal, num = 1) {
+shinchoku_test <- function (goal = 1, num = 1) {
   if(file.exists(".gitignore") == TRUE) {
     commit <- system("git log --since='$(date +'%Y-%m-%d') 00:00:00' --no-merges --oneline | wc -l", 
                      intern = T) %>%
@@ -15,7 +15,7 @@ shinchoku_test <- function (goal, num = 1) {
     Sys.sleep(2.0)
     print(paste("Today's commit activity is...", commit, 
                 ifelse(commit < goal,
-                       c("Never give up x("), c("So nice! Keep it up :)")), 
+                       c("Why don't you do your best?"), c("So nice! Keep it up :)")), 
                 sep = " "))
   } else {
     print("sorry, absence .git directly.")
